be97b563b83a4f200ae42a23e1105e1eb73fe5e5
Ramesh <ramesh.grails@gmail.com>
2022-06-30 01:15:50 +0530

    test

---
+++ src/main/java/com/springweb/ProductstApiApplication.java
@@ -0,0 +1,12 @@
+package com.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductestapiApplication.class, args);     
+	}  
+}
